93576b68873811351121ae8a8d8a55776061492f
Ryan Lee <seungjaeryanlee@gmail.com>
2022-12-24 13:15:07 +0000

    Try using albumentations

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -1,16 +1,22 @@
 import json
 import os
 
+import cv2
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 
 
 class CIFAR10LTDataset(Dataset):
     def __init__(self, json_filepath, images_dirpath, transform=None, target_transform=None):
+        """
+        CIFAR-10-LT Dataset
+        
+        NOTE: transform is NOT expected to be PyTorch transform, but albumentations transform.
+
+        """
         self.json_filepath = json_filepath
         self.images_dirpath = images_dirpath
         self.transform = transform
-        self.target_transform = target_transform
 
         with open(self.json_filepath, "r")as f:
             self.json_data = json.load(f)
@@ -21,11 +27,14 @@ class CIFAR10LTDataset(Dataset):
     def __getitem__(self, idx):
         label = self.json_data["annotations"][idx]["category_id"]
         image_filepath = self.json_data["annotations"][idx]["fpath"]
-        image = read_image(image_filepath)
+        # NOTE: Below line reads image as PyTorch Tensor
+        # image = read_image(image_filepath)
+        # NOTE: Instead, we use OpenCV to use albumentations library
+        image = cv2.imread(image_filepath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
+            augmented = self.transform(image=image)
+            image = augmented["image"]
 
         return image, label
