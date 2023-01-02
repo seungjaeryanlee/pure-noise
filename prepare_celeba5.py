728e54e466cc1dbbf0372b843d7114cd8c79b2c9
briansmlee <briansmlee@gmail.com>
2023-01-02 06:41:03 -0500

    Add prepare_celeba5.py

---
+++ prepare_celeba5.py
@@ -0,0 +1,32 @@
+"""
+Script to download CelebA-5 dataset.
+"""
+import os
+import zipfile
+import gdown
+
+def download_from_gdrive():
+    """
+    Download CelebA-5 dataset from M2m (Kim et al., 2020), which is based on the CelebA dataset (Liu et al., 2015).
+
+    These are In-The-Wild Images from CelebA.
+
+    https://github.com/alinlab/M2m
+    https://mmlab.ie.cuhk.edu.hk/projects/CelebA.html
+    """
+    # Download from Google Drive
+    zip_file_name = "CelebA5.zip"
+    url = "https://drive.google.com/uc?id=1Ks_sBVDmcgXG-ALdzTp1aqpYpUHoiY4A"
+    gdown.download(url, zip_file_name, quiet=False)
+
+    # Unzip
+    os.makedirs("data/", exist_ok=True)
+    with zipfile.ZipFile(zip_file_name, "r") as zip_ref:
+        zip_ref.extractall("data/")
+    os.remove(zip_file_name)
+
+def main():
+    download_from_gdrive()
+
+if __name__ == "__main__":
+    main()
