12c8add278633e5f9e7e0c0dfb61e3b736ae899a
Ryan Lee <seungjaeryanlee@gmail.com>
2023-01-07 18:21:58 +0000

    Use 64x64 CelebA dataset

--- datasets/celeba5.py
+++ datasets/celeba5.py
@@ -9,33 +9,29 @@ class CelebA5Dataset(Dataset):
         self.dataset_path = dataset_path
         self.transform = transform
         self.target_transform = target_transform
-        self.img_path_and_labels = []
+        self.img_and_labels = []
 
         labels_path = os.path.join(self.dataset_path, 'labels.txt')
         with open(labels_path, "r") as f:
             for line in f:
                 img_path, label = line.split()
                 img_path = os.path.join(self.dataset_path, img_path)
+                img = read_image(img_path)
                 label = int(label)
-                self.img_path_and_labels.append((img_path, label))
+                self.img_and_labels.append((img, label))
 
     def __len__(self):
-        return len(self.img_path_and_labels)
+        return len(self.img_and_labels)
 
     def __getitem__(self, idx):
-        img_path, label = self.img_path_and_labels[idx]
-        img = read_image(img_path)
-
-        if self.transform:
-            img = self.transform(img)
-        if self.target_transform:
-            label = self.target_transform(label)
-
-        return img, label
+        img, label = self.img_and_labels[idx]
+        out_img = self.transform(img) if self.transform else img
+        out_label = self.target_transform(label) if self.target_transform else label
+        return out_img, out_label
 
 
 if __name__ == "__main__":
-    train_dataset = CelebA5Dataset('data/CelebA5/train')
+    train_dataset = CelebA5Dataset('data/CelebA5_64x64/train')
     print(len(train_dataset))
     img, label = train_dataset[0]
     print(f'label: {label}, image shape: {img.shape}')
--- prepare_celeba5.py
+++ prepare_celeba5.py
@@ -15,8 +15,8 @@ def download_from_gdrive():
     https://mmlab.ie.cuhk.edu.hk/projects/CelebA.html
     """
     # Download from Google Drive
-    zip_file_name = "CelebA5.zip"
-    url = "https://drive.google.com/uc?id=1Ks_sBVDmcgXG-ALdzTp1aqpYpUHoiY4A"
+    zip_file_name = "CelebA5_64x64.zip"
+    url = "https://drive.google.com/uc?id=1l3FoP44bd9U0xtq74OLKcF0JTF_XSecF"
     gdown.download(url, zip_file_name, quiet=False)
 
     # Unzip
