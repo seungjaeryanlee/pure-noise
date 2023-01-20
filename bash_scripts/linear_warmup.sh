2a9152c4391624aaa44745925c552c1b079194fc
Brian Lee <briansmlee@gmail.com>
2023-01-20 22:32:26 +0000

    Add linear warmup bash

---
+++ bash_scripts/linear_warmup.sh
@@ -0,0 +1,12 @@
+# ERM
+python train.py enable_checkpoint=True enable_linear_warmup=True
+
+# OPeN
+# python train.py \
+# load_ckpt=True load_ckpt_filepath='checkpoints/bumbling-vortex-221__epoch_160.pt' load_ckpt_epoch=160 \
+# enable_oversampling=True enable_open=True
+
+# Oversampling
+# python train.py \
+# load_ckpt=True load_ckpt_filepath='checkpoints/bumbling-vortex-221__epoch_160.pt' load_ckpt_epoch=160 \
+# enable_oversampling=True
