0cdfcb0691440805a83a78a8bbc720a863338ab9
Brian Lee <briansmlee@gmail.com>
2023-01-22 02:41:41 +0000

    Add Cutout aug

--- bash_scripts/linear_warmup.sh
+++ bash_scripts/linear_warmup.sh
@@ -1,3 +1,19 @@
+### Warmup off
+# ERM
+python train.py enable_linear_warmup=False save_ckpt=True wandb_name='no-warmup-0121'
+
+# Oversampling
+python train.py enable_linear_warmup=False wandb_name='no-warmup-rs-0121' \
+load_ckpt=True load_ckpt_filepath='checkpoints/no-warmup-0121__epoch_159.pt' \
+enable_oversampling=True
+
+# OPeN
+python train.py enable_linear_warmup=False wandb_name='no-warmup-open-0121' \
+load_ckpt=True load_ckpt_filepath='checkpoints/no-warmup-0121__epoch_159.pt' \
+enable_oversampling=True enable_open=True
+
+### Warmup on
+# ERM
 python train.py enable_linear_warmup=True save_ckpt=True wandb_name='warmup-0121'
 
 # Oversampling
