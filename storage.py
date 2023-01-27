31461876ca4c2dd43b112ad8554c249352a9920b
Ryan Lee <seungjaeryanlee@gmail.com>
2023-01-27 05:59:27 +0000

    Plot confusion matrix

---
+++ storage.py
@@ -0,0 +1,5 @@
+CHECKPOINT_URLS = {
+    "no-warmup-open-0121__epoch_199.pt": "https://drive.google.com/uc?id=1ZEaevP97mXYdLU_Jv6S5sm2oR3TZZLeF",
+    "no-warmup-open-majority-0121__epoch_199.pt": "https://drive.google.com/uc?id=1mJrMIjT8qPcDy7ZcY87ak-sWu8hLyE96",
+    "no-warmup-0121__epoch_199.pt": "https://drive.google.com/uc?id=1ekJzJenK0KoF8Y28ulHbWiiuhBpoT5uG",
+}
