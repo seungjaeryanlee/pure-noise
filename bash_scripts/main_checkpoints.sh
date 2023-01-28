4bfabac812945ac0f0d4bb27d4ce9ae43551e1f2
Brian Lee <briansmlee@gmail.com>
2023-01-28 22:07:24 +0000

    Fix bash variable use in main_checkpoints.sh

--- bash_scripts/main_checkpoints.sh
+++ bash_scripts/main_checkpoints.sh
@@ -1,14 +1,14 @@
-WANDB_NAME='zada-0127' 
+WANDB_NAME='normalize-0128'
 
 # ERM
-python train.py wandb_name='${WANDB_NAME}'
+python train.py wandb_name="${WANDB_NAME}"
 
 # Oversampling
-python train.py wandb_name='${WANDB_NAME}-rs' \
-load_ckpt=True load_ckpt_filepath='checkpoints/${WANDB_NAME}__epoch_159.pt' \
+python train.py wandb_name="${WANDB_NAME}-rs" \
+load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
 enable_oversampling=True
 
 # OPeN
-python train.py wandb_name='${WANDB_NAME}-open' \
-load_ckpt=True load_ckpt_filepath='checkpoints/${WANDB_NAME}__epoch_159.pt' \
+python train.py wandb_name="${WANDB_NAME}-open" \
+load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
 enable_oversampling=True enable_open=True
