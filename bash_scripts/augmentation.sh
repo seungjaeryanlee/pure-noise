37c94fd2e7c00625ba0a2086039ea2dce35c1db8
Brian Lee <briansmlee@gmail.com>
2023-02-01 23:38:53 +0000

    Fix  augmentation.sh

--- bash_scripts/augmentation.sh
+++ bash_scripts/augmentation.sh
@@ -1,8 +1,6 @@
 ### Reproduce Figure 3 (data augmentation ablation study) in the paper.
-# Model: WideResNet 
-# Dataset: CIFAR-10-LT
 
-### HorizontalFlip + RandomCrop
+# ### HorizontalFlip + RandomCrop
 WANDB_NAME='flipcrop-0201'
 
 # ERM
@@ -45,16 +43,16 @@ WANDB_NAME='simclr-0201'
 
 # ERM
 python train.py wandb_name="${WANDB_NAME}" \
-'train_transform_reprs=["RandomHorizontalFlip()","RandomCrop(32, padding=4)","ToTensor()","Cutout(n_holes=1, length=16)","RandomApply([ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8)","RandomGrayscale(p=0.2)","RandomApply([GaussianBlur(kernel_size=3, sigma=[.1, 2.])], p=0.5)]'
+'train_transform_reprs=["RandomHorizontalFlip()","RandomCrop(32, padding=4)","ToTensor()","Cutout(n_holes=1, length=16)","RandomApply([ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8)","RandomGrayscale(p=0.2)","RandomApply([GaussianBlur(kernel_size=3, sigma=[.1, 2.])], p=0.5)"]'
 
 # Oversampling
 python train.py wandb_name="${WANDB_NAME}-rs" \
-'train_transform_reprs=["RandomHorizontalFlip()","RandomCrop(32, padding=4)","ToTensor()","Cutout(n_holes=1, length=16)","RandomApply([ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8)","RandomGrayscale(p=0.2)","RandomApply([GaussianBlur(kernel_size=3, sigma=[.1, 2.])], p=0.5)]' \
+'train_transform_reprs=["RandomHorizontalFlip()","RandomCrop(32, padding=4)","ToTensor()","Cutout(n_holes=1, length=16)","RandomApply([ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8)","RandomGrayscale(p=0.2)","RandomApply([GaussianBlur(kernel_size=3, sigma=[.1, 2.])], p=0.5)"]' \
 load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
 enable_oversampling=True
 
 # OPeN
 python train.py wandb_name="${WANDB_NAME}-open" \
-'train_transform_reprs=["RandomHorizontalFlip()","RandomCrop(32, padding=4)","ToTensor()","Cutout(n_holes=1, length=16)","RandomApply([ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8)","RandomGrayscale(p=0.2)","RandomApply([GaussianBlur(kernel_size=3, sigma=[.1, 2.])], p=0.5)]' \
+'train_transform_reprs=["RandomHorizontalFlip()","RandomCrop(32, padding=4)","ToTensor()","Cutout(n_holes=1, length=16)","RandomApply([ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8)","RandomGrayscale(p=0.2)","RandomApply([GaussianBlur(kernel_size=3, sigma=[.1, 2.])], p=0.5)"]' \
 load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
 enable_oversampling=True enable_open=True
