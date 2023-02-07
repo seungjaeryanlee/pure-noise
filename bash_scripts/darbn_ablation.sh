64fb1a1bd41692b1f1a3577591ae374d329469f7
Brian Lee <briansmlee@gmail.com>
2023-02-06 22:22:07 -0500

    Add BN ablation with ResNEt

--- bash_scripts/darbn_ablation.sh
+++ bash_scripts/darbn_ablation.sh
@@ -10,3 +10,14 @@ wandb_name="${WANDB_NAME}-standard" noise_bn_option='STANDARD'
 
 python train.py enable_oversampling=True enable_open=True \
 wandb_name="${WANDB_NAME}-darbn" noise_bn_option='DARBN'
+
+WANDB_NAME='BnAblationResNet'
+
+python train.py enable_oversampling=True enable_open=True model='ResNet-32-akamaster' \
+wandb_name="${WANDB_NAME}-auxbn" noise_bn_option='AUXBN'
+
+python train.py enable_oversampling=True enable_open=True model='ResNet-32-akamaster' \
+wandb_name="${WANDB_NAME}-standard" noise_bn_option='STANDARD'
+
+python train.py enable_oversampling=True enable_open=True model='ResNet-32-akamaster' \
+wandb_name="${WANDB_NAME}-darbn" noise_bn_option='DARBN'
