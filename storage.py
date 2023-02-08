186ac55e854d8f92c3dc90f331a16ce876c7420c
Ryan Lee <seungjaeryanlee@gmail.com>
2023-02-08 02:49:02 +0000

    Update analysis notebooks

--- storage.py
+++ storage.py
@@ -2,4 +2,7 @@ CHECKPOINT_URLS = {
     "no-warmup-open-0121__epoch_199.pt": "https://drive.google.com/uc?id=1ZEaevP97mXYdLU_Jv6S5sm2oR3TZZLeF",
     "no-warmup-open-majority-0121__epoch_199.pt": "https://drive.google.com/uc?id=1mJrMIjT8qPcDy7ZcY87ak-sWu8hLyE96",
     "no-warmup-0121__epoch_199.pt": "https://drive.google.com/uc?id=1ekJzJenK0KoF8Y28ulHbWiiuhBpoT5uG",
+    "cui-indices__epoch_159.pt": "https://drive.google.com/uc?id=1WJ0TSmQREZhQo0Vv65mO886Jbfvx-5Ty",
+    "m2m-indices__epoch_159.pt": "https://drive.google.com/uc?id=130Deyqxeu26PfWkDV3G5Qr74Vg6Vq6PF",
+    "ldam-indices__epoch_159.pt": "https://drive.google.com/uc?id=1v1STO9hu-wDon2vsA7Sx08iajuiG5OdC",
 }
