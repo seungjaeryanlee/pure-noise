6c847f4ac72ed1b74246d37e8680b13dae0c0f2b
Brian Lee <briansmlee@gmail.com>
2023-02-08 05:52:04 +0000

    Table 1 bash

--- bash_scripts/datasets_and_ir_ratios.sh
+++ bash_scripts/datasets_and_ir_ratios.sh
@@ -1,14 +1,24 @@
-# CIFAR10LT: IR=50,100
-# CIFAR100-LT: IR=50,100
-# CELEBA5
+# Reproduce Table 1.
+# Compare ERM, (non-deferred) oversampling, and OPeN on
+# CIFAR-10-LT with IR=50,100 and CIFAR-100-LT with IR=50,100.
+
+WANDB_NAME="CIFAR10IR100"
+
+python train.py dataset="CIFAR-10-LT" ir_ratio=100 wandb_name="${WANDB_NAME}"
+
+python train.py dataset="CIFAR-10-LT" ir_ratio=100 wandb_name="${WANDB_NAME}-rs" \
+enable_oversampling=True oversampling_start_epoch=0
+
+python train.py dataset="CIFAR-10-LT" ir_ratio=100 wandb_name="${WANDB_NAME}-open" \
+load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
+enable_oversampling=True enable_open=True
 
 WANDB_NAME="CIFAR10IR50"
 
 python train.py dataset="CIFAR-10-LT" ir_ratio=50 wandb_name="${WANDB_NAME}"
 
 python train.py dataset="CIFAR-10-LT" ir_ratio=50 wandb_name="${WANDB_NAME}-rs" \
-load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
-enable_oversampling=True
+enable_oversampling=True oversampling_start_epoch=0
 
 python train.py dataset="CIFAR-10-LT" ir_ratio=50 wandb_name="${WANDB_NAME}-open" \
 load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
@@ -19,8 +29,7 @@ WANDB_NAME="CIFAR100IR100"
 python train.py dataset="CIFAR-100-LT" ir_rato=100 wandb_name="${WANDB_NAME}"
 
 python train.py dataset="CIFAR-100-LT" ir_rato=100 wandb_name="${WANDB_NAME}-rs" \
-load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
-enable_oversampling=True
+enable_oversampling=True oversampling_start_epoch=0
 
 python train.py dataset="CIFAR-100-LT" ir_rato=100 wandb_name="${WANDB_NAME}-open" \
 load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
@@ -31,8 +40,7 @@ WANDB_NAME="CIFAR100IR50"
 python train.py dataset="CIFAR-100-LT" ir_rato=50 wandb_name="${WANDB_NAME}"
 
 python train.py dataset="CIFAR-100-LT" ir_rato=50 wandb_name="${WANDB_NAME}-rs" \
-load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
-enable_oversampling=True
+enable_oversampling=True oversampling_start_epoch=0
 
 python train.py dataset="CIFAR-100-LT" ir_rato=50 wandb_name="${WANDB_NAME}-open" \
 load_ckpt=True load_ckpt_filepath="checkpoints/${WANDB_NAME}__epoch_159.pt" \
