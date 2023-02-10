c249c9e3e9a6bb35c943d8a57426826783e2b2df
Ryan Lee <seungjaeryanlee@gmail.com>
2023-02-10 19:46:55 +0000

    Remove unused bash scripts

--- bash_scripts/main.sh
+++
@@ -1,6 +0,0 @@
-### Run ERM, Oversampling, and OPeN. 
-# Model: WideResNet
-# Dataset: CIFAR-10-LT
-python train.py
-python train.py enable_oversampling=True
-python train.py enable_oversampling=True enable_open=True
